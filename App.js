a93614327b83cf5d9475cad6761d47f01b99abab
Samuel Stephen @samuel-dev1 <samuelyyyy257@gmail.com>
2022-08-09 06:09:32 -0700

    this is Daily App Clone

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
+import { StyleSheet, Text, View, StatusBar} from 'react-native';
+import { EvilIcons } from '@expo/vector-icons';
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <EvilIcons name="search" size={30} />
+      <EvilIcons name="" size={30} />
+      <StatusBar />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
